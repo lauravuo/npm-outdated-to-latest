fa0b22876ef9e15d17de8f9bb713af2d0ee2735e
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2020-09-28 18:05:15 +0300

    Add bin field.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const { exec } = require("child_process");
 const { preview } = require("./src/previewer")();
 const { clean, update, finalize } = require("./src/executor")();
